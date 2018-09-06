f3eda80b23c410842ef2b41cea79cbacc601100f
plee19 <42356593+plee19@users.noreply.github.com>
2018-09-06 11:55:26 -0500

    Project completed, pending javadoc/testing

--- src/us/plee19/Main.java
+++ src/us/plee19/Main.java
@@ -12,10 +12,11 @@ public class Main {
     private final static FileInput cardPurchases = new FileInput("movie_purchases.csv");
     private final static FileInput movieRating = new FileInput("movie_rating.csv");
     private final static FileOutput outFile = new FileOutput("output_file.csv");
+    private final static FileOutput ratingsSum = new FileOutput("ratings_sum.txt");
     private static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
-        /*String line;
+        String line;
         String[] fields;
         int[] nums = new int[2];
         int[] ratings = new int[2];
@@ -27,18 +28,38 @@ public class Main {
             findAvgRating(fields[0], ratings);
 
             // Set up file for input of above data
-            System.out.printf("00%6s,%-18s,%2d,%4d,%8.2f\n",fields[0],fields[1],nums[0], nums[1], (float)ratings[1]/ratings[0]);
-            String outputString = String.format("00%s,%s,%d,%d,%.2f",fields[0],fields[1],nums[0], nums[1], (float)ratings[1]/ratings[0]);
+            System.out.printf("00%6s  %-18s  %2d    %4d  %8.2f\n", fields[0], fields[1], nums[0], nums[1], (float) ratings[1] / ratings[0]);
+            String outputString = String.format("00%s,%s,%d,%d,%.2f", fields[0], fields[1], nums[0], nums[1], (float) ratings[1] / ratings[0]);
+
             outFile.fileWrite(outputString);
-
-
         }
-        outFile.fileClose();*/
+        outFile.fileClose();
+
         ArrayList<Integer> ratingsArray = new ArrayList<>(buildRatingsArray());
         Collections.sort(ratingsArray);
+        int oneCount = Collections.frequency(ratingsArray, 1);
+        int twoCount = Collections.frequency(ratingsArray, 2);
+        int threeCount = Collections.frequency(ratingsArray, 3);
+        int fourCount = Collections.frequency(ratingsArray, 4);
+        int fiveCount = Collections.frequency(ratingsArray, 5);
+        String results1 = "Rating Count";
+        String results2 = 1 + "      " + oneCount;
+        String results3 = 2 + "      " + twoCount;
+        String results4 = 3 + "      " + threeCount;
+        String results5 = 4 + "      " + fourCount;
+        String results6 = 5 + "      " + fiveCount;
+        ratingsSum.fileWrite(results1);
+        ratingsSum.fileWrite(results2);
+        ratingsSum.fileWrite(results3);
+        ratingsSum.fileWrite(results4);
+        ratingsSum.fileWrite(results5);
+        ratingsSum.fileWrite(results6);
+
+        ratingsSum.fileClose();
 
 
         // Open file for input and print a line for each movie rating, with count/total
+
     }
 
     public static void findPurchases(String acct, int[] nums) {
